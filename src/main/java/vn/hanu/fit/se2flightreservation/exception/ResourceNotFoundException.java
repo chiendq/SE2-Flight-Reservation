a9457d8d340d9a44790ba0cfa418e1cf67f234c6
chiendq <chiendq.work@gmail.com>
2022-04-17 11:11:20 +0700

    more detail with exception

--- src/main/java/vn/hanu/fit/se2flightreservation/exception/ResourceNotFoundException.java
+++ src/main/java/vn/hanu/fit/se2flightreservation/exception/ResourceNotFoundException.java
@@ -1,18 +1,28 @@
 package vn.hanu.fit.se2flightreservation.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.function.Supplier;
-
+@Getter
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends Exception {
+public class ResourceNotFoundException extends RuntimeException {
     private static final Long serialVersionUID = 1L;
+
+    private String resourceName;
+    private String fieldName;
+    private Object filedValue;
 
     public ResourceNotFoundException(String message) {
         super(message);
     }
 
 
+    public ResourceNotFoundException(String resourceName, String fieldName, Object filedValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, filedValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.filedValue = filedValue;
+    }
 }
 
