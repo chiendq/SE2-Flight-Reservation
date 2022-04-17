21e911e31a1319338eb0f401da7743e65fa60a57
chiendq <chiendq.work@gmail.com>
2022-04-17 13:46:16 +0700

    create AirportService

---
+++ src/main/java/vn/hanu/fit/se2flightreservation/services/AirportService.java
@@ -0,0 +1,20 @@
+package vn.hanu.fit.se2flightreservation.services;
+
+import vn.hanu.fit.se2flightreservation.entity.Airport;
+
+import java.util.List;
+
+public interface AirportService {
+
+    Airport save(Airport airport);
+
+    List<Airport> getAllAirports();
+
+    Airport getAirportById(int id);
+
+    Airport updateAirport(Airport airport, int id);
+
+    void deleteAirportById(int id);
+
+    Airport isPresent(int id);
+}
