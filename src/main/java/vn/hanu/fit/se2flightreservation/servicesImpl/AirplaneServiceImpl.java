13d9015385536b340f29edefbd005052b68b6fa1
chiendq <chiendq.work@gmail.com>
2022-04-17 14:35:59 +0700

    init AirplaneServiceImpl

---
+++ src/main/java/vn/hanu/fit/se2flightreservation/servicesImpl/AirplaneServiceImpl.java
@@ -0,0 +1,55 @@
+package vn.hanu.fit.se2flightreservation.servicesImpl;
+
+import org.springframework.stereotype.Service;
+import vn.hanu.fit.se2flightreservation.entity.Airplane;
+
+import vn.hanu.fit.se2flightreservation.exception.EntityExistedByIdException;
+import vn.hanu.fit.se2flightreservation.exception.ResourceNotFoundException;
+import vn.hanu.fit.se2flightreservation.repository.AirplaneRepository;
+import vn.hanu.fit.se2flightreservation.services.AirplaneService;
+
+import java.util.List;
+
+@Service
+public class AirplaneServiceImpl implements AirplaneService {
+    private AirplaneRepository airplaneRepository;
+
+    public AirplaneServiceImpl(AirplaneRepository airplaneRepository){
+        this.airplaneRepository = airplaneRepository;
+    }
+
+    @Override
+    public Airplane save(Airplane airplane) {
+        int id = airplane.getId();
+        if(airplaneRepository.existsById(id)){
+            throw new EntityExistedByIdException("Airplane","Id", id);
+        }
+        return airplaneRepository.save(airplane);
+    }
+
+    @Override
+    public List<Airplane> getAllAirplanes() {
+        return airplaneRepository.findAll();
+    }
+
+    @Override
+    public Airplane getAirplaneById(int id) {
+        return airplaneRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Airplane", "Id", id));
+    }
+
+    @Override
+    public Airplane updateAirplane(Airplane airplane, int id) {
+        Airplane existingAirplane = airplaneRepository.findById(id).orElseThrow(
+                ()-> new ResourceNotFoundException("Airplane","Id",id));
+        existingAirplane.setCode(airplane.getCode());
+        return airplaneRepository.save(existingAirplane);
+    }
+
+    @Override
+    public void deleteAirplaneById(int id) {
+        Airplane existingAirplane = airplaneRepository.findById(id).orElseThrow(
+                ()-> new ResourceNotFoundException("Airplane","Id",id));
+        airplaneRepository.delete(existingAirplane);
+    }
+}
