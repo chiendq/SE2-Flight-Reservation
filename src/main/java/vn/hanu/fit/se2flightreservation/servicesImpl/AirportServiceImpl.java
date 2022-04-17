a6e25a5fdcf1c953cfe6e0362a9d1b703c38f342
chiendq <chiendq.work@gmail.com>
2022-04-17 13:51:43 +0700

    update AirportServiceImpl

--- src/main/java/vn/hanu/fit/se2flightreservation/servicesImpl/AirportServiceImpl.java
+++ src/main/java/vn/hanu/fit/se2flightreservation/servicesImpl/AirportServiceImpl.java
@@ -1,27 +1,62 @@
 package vn.hanu.fit.se2flightreservation.servicesImpl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import vn.hanu.fit.se2flightreservation.entity.Airline;
 import vn.hanu.fit.se2flightreservation.entity.Airport;
+import vn.hanu.fit.se2flightreservation.exception.EntityExistedByIdException;
+import vn.hanu.fit.se2flightreservation.exception.ResourceNotFoundException;
 import vn.hanu.fit.se2flightreservation.repository.AirportRepository;
+import vn.hanu.fit.se2flightreservation.services.AirportService;
 
 import java.util.List;
 
 @Service
-public class AirportServiceImpl {
-    @Autowired
-    AirportRepository airportRepository;
+public class AirportServiceImpl implements AirportService {
+    private AirportRepository airportRepository;
 
-    public List<Airport> getAll(){
-        return airportRepository.findAll();
+    public AirportServiceImpl(AirportRepository airportRepository){
+        this.airportRepository = airportRepository;
     }
 
-    public Airport getById(int id) {
-        return airportRepository.findById(id).get();
-    }
-
+    @Override
     public Airport save(Airport airport) {
+        int id = airport.getId();
+        if(isPresent(id)!= null){
+            throw new EntityExistedByIdException("Airline","Id", id);
+        }
         return airportRepository.save(airport);
     }
 
+    @Override
+    public List<Airport> getAllAirports() {
+        return airportRepository.findAll();
+    }
+
+    @Override
+    public Airport getAirportById(int id) {
+        return airportRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Airport", "Id", id));
+    }
+
+    @Override
+    public Airport updateAirport(Airport airport, int id) {
+        Airport existingAirport = isPresent(id);
+        existingAirport.setCity(airport.getCity());
+        existingAirport.setCode(airport.getCode());
+        existingAirport.setName(airport.getName());
+        return airportRepository.save(existingAirport);
+    }
+
+    @Override
+    public void deleteAirportById(int id) {
+        Airport existingAirport = isPresent(id);
+        airportRepository.delete(existingAirport);
+    }
+
+    @Override
+    public Airport isPresent(int id) {
+        Airport existingAirport = airportRepository.findById(id).orElseThrow(
+                ()-> new ResourceNotFoundException("Airline","Id",id));
+        return existingAirport;
+    }
 }
