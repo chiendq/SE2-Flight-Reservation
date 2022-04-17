1b64f26a8678dc05e7852465a45b78b3cb4bcc06
chiendq <chiendq.work@gmail.com>
2022-04-17 11:08:43 +0700

    create service layer

---
+++ src/main/java/vn/hanu/fit/se2flightreservation/services/AirlineService.java
@@ -0,0 +1,19 @@
+package vn.hanu.fit.se2flightreservation.services;
+
+import vn.hanu.fit.se2flightreservation.entity.Airline;
+
+import java.util.List;
+
+public interface AirlineService {
+    Airline save(Airline airline);
+
+    List<Airline> getAllAirlines();
+
+    Airline getAirlineById(int id);
+
+    Airline updateAirline(Airline airline, int id);
+
+    void deleteAirlineById(int id);
+
+    Airline isPresent(int id);
+}
