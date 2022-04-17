230bd654fb416c9cd03532161afc91be5f1ee524
chiendq <chiendq.work@gmail.com>
2022-04-17 14:29:19 +0700

    update

--- src/main/java/vn/hanu/fit/se2flightreservation/repository/AirplaneRepository.java
+++ src/main/java/vn/hanu/fit/se2flightreservation/repository/AirplaneRepository.java
@@ -3,5 +3,6 @@ package vn.hanu.fit.se2flightreservation.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import vn.hanu.fit.se2flightreservation.entity.Airplane;
 
-public interface AirplaneRepository extends JpaRepository<Airplane, Integer> {
+public interface AirplaneRepository
+        extends JpaRepository<Airplane, Integer> {
 }
