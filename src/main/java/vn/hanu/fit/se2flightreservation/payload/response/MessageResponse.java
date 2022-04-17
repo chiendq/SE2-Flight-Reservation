13c14d047851539adb5f1f8e0f2a5cf332cccf07
chiendq <chiendq.work@gmail.com>
2022-04-17 15:22:47 +0700

    add payload package

--- src/main/java/vn/hanu/fit/se2flightreservation/payload/response/MessageResponse.java
+++ src/main/java/vn/hanu/fit/se2flightreservation/payload/response/MessageResponse.java
@@ -1,14 +1,17 @@
 package vn.hanu.fit.se2flightreservation.payload.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class MessageResponse {
     private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
