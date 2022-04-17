095284306da806c50ca532992d56bc222766e33c
chiendq <chiendq.work@gmail.com>
2022-04-17 14:20:12 +0700

    update FlightClassController

--- src/main/java/vn/hanu/fit/se2flightreservation/controller/FlightClassController.java
+++ src/main/java/vn/hanu/fit/se2flightreservation/controller/FlightClassController.java
@@ -1,32 +1,48 @@
 package vn.hanu.fit.se2flightreservation.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.hanu.fit.se2flightreservation.entity.FlightClass;
-import vn.hanu.fit.se2flightreservation.servicesImpl.FlightClassServiceImpl;
+import vn.hanu.fit.se2flightreservation.services.FlightClassService;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/flightClass")
 public class FlightClassController {
-    @Autowired
-    FlightClassServiceImpl flightClassService;
 
-    @GetMapping("")
-    public List<FlightClass> getAll(){
-        return flightClassService.getAll();
-    }
+    private FlightClassService flightClassService;
 
-    @GetMapping(value = {"/{id}"})
-    public FlightClass getById(@PathVariable(value = "id") int id){
-        return flightClassService.getById(id);
+    public FlightClassController(FlightClassService flightClassService) {
+        super();
+        this.flightClassService = flightClassService;
     }
 
     @PostMapping("")
-    public FlightClass save(@RequestBody FlightClass flightClass){
-        return flightClassService.save(flightClass);
+    public ResponseEntity<FlightClass> saveFlightClass(@RequestBody FlightClass flightClass) {
+        return new ResponseEntity<FlightClass>(flightClassService.save(flightClass), HttpStatus.CREATED);
     }
 
+    @GetMapping("")
+    public List<FlightClass> getAllFlightClasss() {
+        return flightClassService.getAllFlightClasss();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FlightClass> getFlightClassById(@PathVariable("id") int flightClassId) {
+        return new ResponseEntity<FlightClass>(flightClassService.getFlightClassById(flightClassId), HttpStatus.OK);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<FlightClass> updateFlightClass(@PathVariable("id") int id, @RequestBody FlightClass flightClass) {
+        return new ResponseEntity<FlightClass>(flightClassService.updateFlightClass(flightClass, id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteFlightClass(@PathVariable("id") int id) {
+        flightClassService.deleteFlightClassById(id);
+        return new ResponseEntity<String>("FlightClass deleted successfully!.",HttpStatus.OK);
+    }
 
 }
