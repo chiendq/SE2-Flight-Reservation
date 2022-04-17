a66e02582a4b63818046e0c8cd54ba4130e3783e
chiendq <chiendq.work@gmail.com>
2022-04-17 11:11:55 +0700

    add @Repository

--- src/main/java/vn/hanu/fit/se2flightreservation/repository/FlightClassRepository.java
+++ src/main/java/vn/hanu/fit/se2flightreservation/repository/FlightClassRepository.java
@@ -1,10 +1,12 @@
 package vn.hanu.fit.se2flightreservation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import vn.hanu.fit.se2flightreservation.entity.FlightClass;
 
 import java.util.Optional;
 
+@Repository
 public interface FlightClassRepository
         extends JpaRepository<FlightClass, Integer> {
 
--- src/main/java/vn/hanu/fit/se2flightreservation/repository/TicketRepository.java
+++ src/main/java/vn/hanu/fit/se2flightreservation/repository/TicketRepository.java
@@ -1,8 +1,28 @@
 package vn.hanu.fit.se2flightreservation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import vn.hanu.fit.se2flightreservation.entity.Ticket;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+@Repository
 public interface TicketRepository
         extends JpaRepository<Ticket, Integer> {
+    List<Ticket> findAllByArrivalAirport_IdAndDepartureAirport_IdAndFlightClass_Id(int arrivalAirportId,
+                                                                                      int departureAirportId,
+                                                                                      int flightClassId);
+
+    List<Ticket> findAllByDepartureAirport_IdAndArrivalAirport_IdAndFlightClass_IdAndDepartureTime(
+            int departureAirportId,
+            int arrivalAirportId,
+            int flightClassId,
+            Timestamp departureTime
+            );
+
+//    String search = ;
+    @Query(value="SELECT * FROM tickets t where t.flight_class_id= :flightClassId", nativeQuery=true)
+    List<Ticket> getByFlightClassId(int flightClassId);
 }
