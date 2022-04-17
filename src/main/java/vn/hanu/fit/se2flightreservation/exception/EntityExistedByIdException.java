8a5b9a2fa45988bd69023ff99f849a20e1c6bbb6
chiendq <chiendq.work@gmail.com>
2022-04-17 11:10:50 +0700

    create exception for existed entity id

---
+++ src/main/java/vn/hanu/fit/se2flightreservation/exception/EntityExistedByIdException.java
@@ -0,0 +1,30 @@
+package vn.hanu.fit.se2flightreservation.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @<code>422</code>
+ * <p>the server understands the content type of the request entity </p>
+ * <p>but was unable to process the contained instructions.</p>
+ */
+@ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
+public class EntityExistedByIdException extends RuntimeException{
+    private static final Long serialVersionUID = 1L;
+
+    private String resourceName;
+    private String fieldName;
+    private Object filedValue;
+
+    public EntityExistedByIdException(String message) {
+        super(message);
+    }
+
+    public EntityExistedByIdException(String resourceName, String fieldName, Object filedValue) {
+        super(String.format("%s is existed with %s : '%s'", resourceName, fieldName, filedValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.filedValue = filedValue;
+    }
+
+}
