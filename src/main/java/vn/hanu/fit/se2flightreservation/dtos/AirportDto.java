76dfbcdb5e23fcd3c7e8fd25c85b605e7f039d16
chiendq <chiendq.work@gmail.com>
2022-04-18 13:43:58 +0700

    create more dto

---
+++ src/main/java/vn/hanu/fit/se2flightreservation/dtos/AirportDto.java
@@ -0,0 +1,19 @@
+package vn.hanu.fit.se2flightreservation.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AirportDto {
+
+    private String code;
+
+    private String name;
+
+    private String city;
+}
---
+++ src/main/java/vn/hanu/fit/se2flightreservation/dtos/TicketResponseDto.java
@@ -0,0 +1,31 @@
+package vn.hanu.fit.se2flightreservation.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TicketResponseDto {
+    private int id;
+
+    private String airplaneName;
+
+    private String airplaneImgAPI;
+
+    private AirportDto departureAirportDto;
+
+    private AirportDto arrivalAirportDto;
+
+    private String departureDate;
+
+    private String arrivalDate;
+
+    private int cost;
+
+}
