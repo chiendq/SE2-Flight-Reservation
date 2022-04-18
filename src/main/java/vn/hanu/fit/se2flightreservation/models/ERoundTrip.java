04cf08197556f6f112eafaa6ecea067d21158594
chiendq <chiendq.work@gmail.com>
2022-04-18 13:42:20 +0700

    create more two Enum

---
+++ src/main/java/vn/hanu/fit/se2flightreservation/models/ERoundTrip.java
@@ -0,0 +1,6 @@
+package vn.hanu.fit.se2flightreservation.models;
+
+public enum ERoundTrip {
+    ROUND_TRIP_TRUE,
+    ROUND_TRIP_FALSE
+}
---
+++ src/main/java/vn/hanu/fit/se2flightreservation/models/EStatus.java
@@ -0,0 +1,7 @@
+package vn.hanu.fit.se2flightreservation.models;
+
+public enum EStatus {
+    STATUS_AVAILABLE,
+    STATUS_PENDING,
+    STATUS_SOLD
+}
