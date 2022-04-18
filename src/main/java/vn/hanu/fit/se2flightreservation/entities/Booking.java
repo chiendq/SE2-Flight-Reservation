8dea233a16afc7c8f6bde4a52f1400d796e58c6a
chiendq <chiendq.work@gmail.com>
2022-04-18 22:10:01 +0700

    create booking entity

---
+++ src/main/java/vn/hanu/fit/se2flightreservation/entities/Booking.java
@@ -0,0 +1,37 @@
+package vn.hanu.fit.se2flightreservation.entities;
+
+import lombok.Data;
+import vn.hanu.fit.se2flightreservation.models.EPaymentMethod;
+import vn.hanu.fit.se2flightreservation.models.EStatus;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Set;
+
+@Data
+@Table(name = "bookings")
+@Entity
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    private Timestamp createdAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private EStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private EPaymentMethod paymentMethod;
+
+    @OneToMany(mappedBy = "booking")
+    private Set<Ticket> tickets;
+
+
+}
