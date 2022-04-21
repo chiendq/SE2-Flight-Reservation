0be57c05ee6a8ce64b074241b95768eccc0549f1
chiendq <chiendq.work@gmail.com>
2022-04-21 17:59:57 +0700

    create User controller for admin role

---
+++ src/main/java/vn/hanu/fit/se2flightreservation/admin/controllers/UserController.java
@@ -0,0 +1,56 @@
+package vn.hanu.fit.se2flightreservation.admin.controllers;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import vn.hanu.fit.se2flightreservation.admin.services.UserService;
+import vn.hanu.fit.se2flightreservation.admin.services.UserService;
+import vn.hanu.fit.se2flightreservation.entities.User;
+
+import java.util.List;
+
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RestController
+@RequestMapping("/api/v1/admin/users")
+public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @PostMapping("")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        User savedUser = userService.save(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("")
+    @JsonFormat
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") int userId) {
+        User user = userService.getUserById(userId);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") int id, @RequestBody User user) {
+        return new ResponseEntity<>(userService.updateUser(user, id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") int id) {
+        userService.deleteUserById(id);
+        return new ResponseEntity<>("User deleted successfully!.", HttpStatus.OK);
+    }
+}
