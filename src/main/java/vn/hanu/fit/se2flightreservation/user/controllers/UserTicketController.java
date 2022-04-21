2c2f0c6cf3c77d0dd6a2ad302e3c8eda62628a76
chiendq <chiendq.work@gmail.com>
2022-04-21 15:55:13 +0700

    fix conflict bean name with admin ticket controller

--- src/main/java/vn/hanu/fit/se2flightreservation/user/controllers/UserTicketController.java
+++ src/main/java/vn/hanu/fit/se2flightreservation/user/controllers/UserTicketController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = {"http://localhost:3000"})
 @RestController
 @RequestMapping("/api/v1/ticket")
-public class TicketController {
+public class UserTicketController {
 
     @GetMapping("/test")
     public String test(){
