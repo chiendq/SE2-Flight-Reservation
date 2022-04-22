351612358871639bfaf8d4951479eb8ec9e5ddd5
chiendq <chiendq.work@gmail.com>
2022-04-22 22:51:42 +0700

    rename and add saveBooking method

--- src/main/java/vn/hanu/fit/se2flightreservation/user/services/UTicketService.java
+++ src/main/java/vn/hanu/fit/se2flightreservation/user/services/UTicketService.java
@@ -1,14 +1,17 @@
 package vn.hanu.fit.se2flightreservation.user.services;
 
+import vn.hanu.fit.se2flightreservation.entities.Booking;
 import vn.hanu.fit.se2flightreservation.user.dtos.ticket.TicketSearchDto;
 import vn.hanu.fit.se2flightreservation.entities.Ticket;
 
 import java.text.ParseException;
 import java.util.List;
 
-public interface UserTicketService {
+public interface UTicketService {
 
     List<Ticket> getAll();
 
     List<Ticket> search(TicketSearchDto ticketSearchDto) throws ParseException;
+
+    boolean saveBooking(Booking booking, int id);
 }
