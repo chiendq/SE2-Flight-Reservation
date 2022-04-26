2561e994f9ecf9637582e597b202cee53663b3d3
chiendq <chiendq@gmail.com>
2022-04-26 22:39:59 +0700

    fix role for admin

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -45,8 +45,8 @@ INSERT INTO users (email, fullname, gender, password, phone, username) VALUES ('
 INSERT INTO user_roles (user_id, role_id) VALUES (1, 1);
 INSERT INTO user_roles (user_id, role_id) VALUES (1, 2);
 INSERT INTO user_roles (user_id, role_id) VALUES (1, 3);
-INSERT INTO user_roles (user_id, role_id) VALUES (2, 1);
-INSERT INTO user_roles (user_id, role_id) VALUES (3, 3);
+INSERT INTO user_roles (user_id, role_id) VALUES (2, 3);
+INSERT INTO user_roles (user_id, role_id) VALUES (3, 2);
 
 
 # Init tickets
